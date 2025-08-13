3cee9e056a3df5fe0b59bbdcbe3458f26d8fd72f
alessandrojpl <alessandrojpl@FCD-1779>
2025-08-13 17:39:01 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+using System.Net.Http.Headers;
+using System.Reflection.PortableExecutable;
+
+
+namespace ScreenSound.Banco
+{
+    public class ScreenSoundContext:DbContext
+    {
+        public DbSet<Artista> Artistas { get; set; }
+        public DbSet<Musica> Musicas { get; set; }
+
+        private string connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=ScreenSoundV0;User ID=sa;Password=Senha@2025;Connect Timeout=200;Application Name=ScreenSound;TrustServerCertificate=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+        }
+
+
+        //antigo OD.net
+        /*
+        public SqlConnection ObterConexao()
+        {
+            return new SqlConnection(connectionString);
+        }
+        */
+
+
+       
+
+
+
+    }
+}
---
+++ ScreenSound/Migrations/20250812195551_PopularTabelaMusica.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabelaMusica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Eduardo e Monica", "1984"});
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
